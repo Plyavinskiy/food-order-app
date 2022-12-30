5bb7f4ae4448b962867d749c6a4c2d18f37b19a2
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2022-12-30 21:38:51 +0200

    feat: implement the MealItemForm component

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,7 +1,12 @@
 import classes from "./MealItemForm.module.css";
 
-const MealItemForm = () => {
-  return <div>MealItemForm</div>;
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <input type="text" />
+      <button>+ Add</button>
+    </form>
+  );
 };
 
 export default MealItemForm;
